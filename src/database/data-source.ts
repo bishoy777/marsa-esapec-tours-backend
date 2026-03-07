c00651e32c31028eacc67df8a4969260f92d1ee3
Zakaria Rezk <zakariarezk0@gmail.com>
2026-03-07 23:37:01 +0200

    change migrations

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,13 +6,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-
+  url: process.env.DB_URL,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/database/migrations/*.js'],
 
