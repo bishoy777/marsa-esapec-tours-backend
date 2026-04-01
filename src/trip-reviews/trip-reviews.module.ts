512b1fbbe36470ea825c125421d42f79bc98cee3
zakaria rezk <zakariarezk0@gmail.com>
2026-04-01 20:39:59 +0200

    tirpreview

--- src/trip-reviews/trip-reviews.module.ts
+++ src/trip-reviews/trip-reviews.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TripReviewsService } from './trip-reviews.service';
 import { TripReviewsController } from './trip-reviews.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Review } from './entities/trip-review.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Review])],
   controllers: [TripReviewsController],
   providers: [TripReviewsService],
 })
