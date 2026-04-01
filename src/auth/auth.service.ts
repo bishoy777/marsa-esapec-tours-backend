23c7e21368f631256c60a617d7c7d0a477269856
zakaria rezk <zakariarezk0@gmail.com>
2026-04-01 14:39:28 +0200

    asdasd

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,7 +22,10 @@ export class AuthService {
     });
 
     if (!userExists) {
-      throw new HttpException('User not found', 404);
+      throw new HttpException(
+        await this.i18n.translate('common.USER_NOT_FOUND'),
+        404,
+      );
     }
 
     const passwordMatch = await compare(
