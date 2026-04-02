624fd3662b4d0e340a2a1ab1d180854b94f56d0a
zakaria rezk <zakariarezk0@gmail.com>
2026-04-02 20:59:58 +0200

    add entity

--- src/trip/trip.module.ts
+++ src/trip/trip.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { Trip } from './entities/trip.entity';
 import { TripImage } from '@/trip/entities/trip-image.entity';
-
+import { TripDay } from '@/trip/entities/trip-day.entity';
 import { TripService } from './trip.service';
 import { TripController } from './trip.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [TypeOrmModule.forFeature([Trip, TripImage])],
+  imports: [TypeOrmModule.forFeature([Trip, TripImage, TripDay])],
   controllers: [TripController],
   providers: [TripService],
   exports: [TripService, TypeOrmModule],
